bef12037118a19b12622f47ad6129a470edd1b84
Oloyede Ishola <isholao120@gmail.com>
2026-08-08 07:41:38 +0100

    Delete app directory

--- app/admin/account/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChangePasswordForm from "@/components/account/ChangePasswordForm";
-
-export default function AccountPage() {
-  return (
-    <div className="p-5 sm:p-8 lg:p-10">
-      <ChangePasswordForm />
-    </div>
-  );
-}
